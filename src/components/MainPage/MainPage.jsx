5ab699196a8b26cb316d44741e1112144c64fbd9
FirdeusGj <firdegjepali@gmail.com>
2023-05-29 10:52:01 +0200

    fixed the main page

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -6,15 +6,21 @@ export default function MainPage() {
       <div className="section__wrapper">
         <div className="section__content--wrapper">
           <div className="section__content">
-            <h1>Book a flight now</h1>
+            <h1>What we have :</h1>
+            <ul className="section__ul">
+              <li className="section__li">Best Hotels</li>
+              <li className="section__li">Cheap Flights</li>
+              <li className="section__li">Best Deals</li>
+              <li className="section__li">Top rated places</li>
+            </ul>
           </div>
-        </div>
-        <div>
-          <img
-            className="mainPage__img"
-            src="https://freepngimg.com/save/26315-airplane-transparent/1000x421"
-            alt=""
-          />
+          <div>
+            <img
+              className="mainPage__img"
+              src="https://freepngimg.com/save/26315-airplane-transparent/1000x421"
+              alt=""
+            />
+          </div>
         </div>
       </div>
     </section>
