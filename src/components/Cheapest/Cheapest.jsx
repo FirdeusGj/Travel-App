c8345bc237012c10a90fd8ed7611ea1f614842c5
FirdeusGj <firdegjepali@gmail.com>
2023-05-31 23:05:57 +0200

    made a little bit of the other section

--- src/components/Cheapest/Cheapest.jsx
+++ src/components/Cheapest/Cheapest.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
-import './Cheapest.css'
+import React from "react";
+import "./Cheapest.css";
 
 export default function Cheapest() {
   return (
-    <div className='cheap'>Cheapest</div>
-  )
+    <section className="cheap">
+      <div className="cheap__content--wrapper">
+        <div>
+          <h1>
+            <span className="stroke__text2">Cheapest Deals</span>
+          </h1>
+        </div>
+        <div>
+
+        </div>
+      </div>
+    </section>
+  );
 }
