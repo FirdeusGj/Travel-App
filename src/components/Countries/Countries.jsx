71bd71a41eb9cc093c4fbdefd7cfaab49886a145
FirdeusGj <firdegjepali@gmail.com>
2023-06-03 22:48:41 +0200

    added another folder

---
+++ src/components/Countries/Countries.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Countries.css'
+export default function Countries() {
+  return (
+    <section>
+        <div>
+            <div>
+                <div>
+                    <input type="text" />
+                    <button>Search</button>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
