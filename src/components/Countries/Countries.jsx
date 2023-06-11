2d63b9ecf3bc49fb4d0963137108752db13b1768
FirdeusGj <firdegjepali@gmail.com>
2023-06-11 23:02:03 +0200

    fixing the country search

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Countries.css'
+import axios from 'axios';
 
 export default function Countries() {
+  const [countries, setCountries] = useState([])
+  const [searchCountry, setSearchCountry] = useState('')
+
+  async function fetchCountries(query){
+    const { data } = await axios.get(
+      `https://restcountries.com/v3.1/name/${searchCountry || query}`
+    );
+    setCountries(data[0].Countries)
+    console.log(data[0].name.common)
+  }
+  console.log(fetchCountries())
+  const searchEnter = (e) => {
+    if(e.key === 'Enter'){
+      fetchCountries();
+    }
+  }
   return (
     <section className='countries'>
         <div>
@@ -11,7 +28,11 @@ export default function Countries() {
                   <p>Get information about a specific country if you're looking to travel there</p>
                 </div>
                 <div>
-                    <input type="text" />
+                    <input type="text"
+                    value={searchCountry}
+                    placeholder='Enter Country'
+                    onChange={(event) => setSearchCountry(event.target.value)}
+                    onKeyUp={searchEnter} />
                     <button>Search</button>
                 </div>
             </div>
